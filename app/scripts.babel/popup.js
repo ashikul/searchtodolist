9a1c64504f52a9cb5770d4c12e876c88c5c0c2db
ashikul <alam.ashikul@gmail.com>
2017-11-09 04:09:21 -0500

    arggh have to do this agian..

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,3 +1,26 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Popup');
+$(document).ready(function () {
+
+  console.log('app ready');
+
+  localStorageSetup();
+
+
+  $('#complete-all').click(function (e) {});
+  $('#search-all').click(function (e) {});
+  $('#delete-all').click(function (e) {});
+  $('#add').click(function (e) {});
+  $('#check').click(function (e) {});
+  $('#delete').click(function (e) {});
+
+
+});
+
+function localStorageSetup() {
+
+  console.log('localStorageSetup');
+  console.log(localStorage);
+  if (!localStorage.tasks) localStorage.tasks = '[]';
+
+}
