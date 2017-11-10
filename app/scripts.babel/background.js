2f876f796a62b180a99d18a8b902af4f03fa783c
ashikul <alam.ashikul@gmail.com>
2017-11-09 23:42:19 -0500

    gonna try diff approach..

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -4,6 +4,25 @@ chrome.runtime.onInstalled.addListener(details => {
   console.log('previousVersion', details.previousVersion);
 });
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
+chrome.browserAction.setBadgeText({
+  text: '\'Allo'
+});
 
 console.log('\'Allo \'Allo! Event Page for Browser Action');
+
+chrome.browserAction.setBadgeBackgroundColor({
+  color: '#777'
+});
+
+
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+  for (key in changes) {
+    var storageChange = changes[key];
+    console.log('Storage key "%s" in namespace "%s" changed. ' +
+      'Old value was "%s", new value is "%s".',
+      key,
+      namespace,
+      storageChange.oldValue,
+      storageChange.newValue);
+  }
+});
--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -2,14 +2,34 @@
 
 $(document).ready(function () {
 
-  console.log('app ready');
+  console.log('app ready 2');
 
   localStorageSetup();
+  // outputTasks();
+  // badge();  
+  // if($('ul.tasks li').length) $('div.check-all').show();
+  // if($('li.completed').length) $('div.del-completed').show();
+
+  $('#complete-all').click(function (e) {
+    $('li').addClass('completed');
+    //update?
+    //outputtasks?
+    //update badge?
+
+  });
+
+  $('#search-all').click(function () {
 
 
-  $('#complete-all').click(function (e) {});
-  $('#search-all').click(function (e) {});
-  $('#delete-all').click(function (e) {});
+  });
+
+  $('#delete-all').click(function () {
+    // $('li.completed').addClass('remove');
+    // $('li.remove').remove();
+    $('li').remove();
+  });
+  
+    
   $('#add').click(function (e) {});
   $('#check').click(function (e) {});
   $('#delete').click(function (e) {});
@@ -17,10 +37,21 @@ $(document).ready(function () {
 
 });
 
+//todo: output tasks
+//update function?itu
+
 function localStorageSetup() {
 
   console.log('localStorageSetup');
   console.log(localStorage);
   if (!localStorage.tasks) localStorage.tasks = '[]';
 
+  //todo: chrome sync
+
+}
+
+function badge() {
+  chrome.browserAction.setBadgeText({
+    text: '' + $('li:not(.completed)').length + ''
+  });
 }
