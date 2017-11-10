0e9ab6f10ff164a242195a04924a1ab0075d4fbd
ashikul <alam.ashikul@gmail.com>
2017-11-10 02:53:01 -0500

    remove unneeded css and js, hide filters

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,28 +1,28 @@
-'use strict';
+// 'use strict';
 
-chrome.runtime.onInstalled.addListener(details => {
-  console.log('previousVersion', details.previousVersion);
-});
+// chrome.runtime.onInstalled.addListener(details => {
+//   console.log('previousVersion', details.previousVersion);
+// });
 
-chrome.browserAction.setBadgeText({
-  text: '\'Allo'
-});
+// chrome.browserAction.setBadgeText({
+//   text: '\'Allo'
+// });
 
-console.log('\'Allo \'Allo! Event Page for Browser Action');
+// console.log('\'Allo \'Allo! Event Page for Browser Action');
 
-chrome.browserAction.setBadgeBackgroundColor({
-  color: '#777'
-});
+// chrome.browserAction.setBadgeBackgroundColor({
+//   color: '#777'
+// });
 
 
-chrome.storage.onChanged.addListener(function (changes, namespace) {
-  for (key in changes) {
-    var storageChange = changes[key];
-    console.log('Storage key "%s" in namespace "%s" changed. ' +
-      'Old value was "%s", new value is "%s".',
-      key,
-      namespace,
-      storageChange.oldValue,
-      storageChange.newValue);
-  }
-});
+// chrome.storage.onChanged.addListener(function (changes, namespace) {
+//   for (key in changes) {
+//     var storageChange = changes[key];
+//     console.log('Storage key "%s" in namespace "%s" changed. ' +
+//       'Old value was "%s", new value is "%s".',
+//       key,
+//       namespace,
+//       storageChange.oldValue,
+//       storageChange.newValue);
+//   }
+// });
--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,66 +1,66 @@
-'use strict';
+// 'use strict';
 
-$(document).ready(function () {
+// $(document).ready(function () {
 
-  console.log('app ready 2');
+//   console.log('app ready 2');
 
-  localStorageSetup();
-  // outputTasks();
-  // badge();  
-  // if($('ul.tasks li').length) $('div.check-all').show();
-  // if($('li.completed').length) $('div.del-completed').show();
+//   localStorageSetup();
+//   // outputTasks();
+//   // badge();  
+//   // if($('ul.tasks li').length) $('div.check-all').show();
+//   // if($('li.completed').length) $('div.del-completed').show();
 
-  $('#complete-all').click(function (e) {
-    $('li').addClass('completed');
-    //update?
-    //outputtasks?
-    //update badge?
+//   $('#complete-all').click(function (e) {
+//     $('li').addClass('completed');
+//     //update?
+//     //outputtasks?
+//     //update badge?
 
-  });
+//   });
 
-  $('#search-all').click(function () {
-
-
-  });
-
-  $('#delete-all').click(function () {
-    // $('li.completed').addClass('remove');
-    // $('li.remove').remove();
-    $('li').remove();
-  });
-  
-    
-    $('#add').click(function (e) {
-      
-
-    });
-
-    $('#check').click(function (e) {
-      
-    });
-    
-    $('#delete').click(function (e) {
-      
-  });
+//   $('#search-all').click(function () {
 
 
-});
+//   });
 
-//todo: output tasks
-//update function?itu
+//   $('#delete-all').click(function () {
+//     // $('li.completed').addClass('remove');
+//     // $('li.remove').remove();
+//     $('li').remove();
+//   });
 
-function localStorageSetup() {
 
-  console.log('localStorageSetup');
-  console.log(localStorage);
-  if (!localStorage.tasks) localStorage.tasks = '[]';
+//     $('#add').click(function (e) {
 
-  //todo: chrome sync
 
-}
+//     });
 
-function badge() {
-  chrome.browserAction.setBadgeText({
-    text: '' + $('li:not(.completed)').length + ''
-  });
-}
+//     $('#check').click(function (e) {
+
+//     });
+
+//     $('#delete').click(function (e) {
+
+//   });
+
+
+// });
+
+// //todo: output tasks
+// //update function?itu
+
+// function localStorageSetup() {
+
+//   console.log('localStorageSetup');
+//   console.log(localStorage);
+//   if (!localStorage.tasks) localStorage.tasks = '[]';
+
+//   //todo: chrome sync
+
+// }
+
+// function badge() {
+//   chrome.browserAction.setBadgeText({
+//     text: '' + $('li:not(.completed)').length + ''
+//   });
+// }
