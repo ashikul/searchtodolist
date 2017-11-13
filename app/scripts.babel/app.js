1daa8c36a4054c6a0034f9155f95bc6a3159ac05
ashikul <alam.ashikul@gmail.com>
2017-11-13 02:58:01 -0500

    change to window.todo... hope its fine...

--- app/scripts.babel/app.js
+++ app/scripts.babel/app.js
@@ -1,25 +1,53 @@
 /*global app, $on */
 (function () {
-	'use strict';
+  'use strict';
 
-	/**
-	 * Sets up a brand new Todo list.
-	 *
-	 * @param {string} name The name of your new to do list.
-	 */
-	function Todo(name) {
-		this.storage = new app.Store(name);
-		this.model = new app.Model(this.storage);
-		this.template = new app.Template();
-		this.view = new app.View(this.template);
-		this.controller = new app.Controller(this.model, this.view);
-	}
+  /**
+   * Sets up a brand new Todo list.
+   *
+   * @param {string} name The name of your new to do list.
+   */
+  function Todo(name) {
+    this.storage = new app.Store(name);
+    this.model = new app.Model(this.storage);
+    this.template = new app.Template();
+    this.view = new app.View(this.template);
+    this.controller = new app.Controller(this.model, this.view);
+  }
 
-	var todo = new Todo('todos-vanillajs');
+  window.todo = new Todo('todos-search-list');
 
-	function setView() {
-		todo.controller.setView(document.location.hash);
-	}
-	$on(window, 'load', setView);
-	$on(window, 'hashchange', setView);
+  function setView() {
+    todo.controller.setView(document.location.hash);
+  }
+  $on(window, 'load', setView);
+  $on(window, 'hashchange', setView);
+
+
+
+  //chrome api 
+
+  //   chrome.contextMenus.create({
+  //     title: 'Add to To-do Search List: %s',
+  //     contexts: ['page_action'],
+  //     // onclick: window.todo.controller.addItem(),
+  //     onclick: alert(),
+  //   });
+
 })();
+
+// function getword(info) {
+//   // console.log('Word ' + info.selectionText + ' was clicked.');
+//   window.todo.controller.addItem(info.selectionText);
+// }
+
+// chrome.contextMenus.create({
+//   title: 'Add: %s',
+//   contexts: ['selection'],
+//   onclick: getword,
+// });
+
+// chrome.contextMenus.onClicked.addListener(function (clickData) {
+//   alert('appy..' + clickData)
+
+// });
