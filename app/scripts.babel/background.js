c785a3a638f4b7bc2b47c991a1a1592109ed9e6d
ashikul <alam.ashikul@gmail.com>
2017-11-13 02:57:23 -0500

    add back ground js for chrome right click

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,4 +1,4 @@
-// 'use strict';
+'use strict';
 
 // chrome.runtime.onInstalled.addListener(details => {
 //   console.log('previousVersion', details.previousVersion);
@@ -26,3 +26,39 @@
 //       storageChange.newValue);
 //   }
 // });
+
+function addTask(info) {
+  //   console.log('Word ' + info.selectionText + ' was clicked.');
+
+  //if its right clicked from inside the extension  
+  if (window.todo) {
+    window.todo.controller.addItem(info.selectionText);
+  } else {
+    //directly add to the local storage
+    //:TODO: not if this will always work, dependency too  
+    var newItem = {
+      title: info.selectionText.trim(),
+      completed: false
+    };
+
+    var data = JSON.parse(localStorage['todos-search-list']);
+    var todos = data.todos;
+    newItem.id = new Date().getTime();
+
+    todos.push(newItem);
+    localStorage['todos-search-list'] = JSON.stringify(data);
+    chrome.browserAction.setBadgeText({
+      text: '' + todos.length
+    });
+  }
+}
+
+
+chrome.contextMenus.removeAll(function () {
+  chrome.contextMenus.create({
+    id: 'AddTask',
+    title: 'Add "%s"',
+    contexts: ['selection'],
+    onclick: addTask,
+  });
+});
