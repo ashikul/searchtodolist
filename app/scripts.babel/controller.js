e5f0cad671e06a4c64ccfe3b2952ea5c660ce10e
ashikul <alam.ashikul@gmail.com>
2017-11-13 02:58:16 -0500

    just a large comment

--- app/scripts.babel/controller.js
+++ app/scripts.babel/controller.js
@@ -324,6 +324,36 @@
     });
   }
 
+  //TODO: this doesnt work for some reason...
+  // chrome.contextMenus.onClicked.addListener(function (clickData) {
+  //   if (clickData.menuItemId == 'Add') {
+  //     console.log('yooooooo');
+  //     console.log(clickData);
+      // if(isInt(clickData.selectionText)) {
+      //   chrome.storage.sync.get(['total', 'limit'], function (budget) {
+      //     var newTotal = 0;
+      //     if(budget.total) {
+      //       newTotal += parseInt(budget.total);
+      //     }
+
+      //     newTotal += parseInt(clickData.selectionText);
+      //     chrome.storage.sync.set({ 'total': newTotal }, function () {
+      //       if(newTotal >= budget.limit) {
+      //         var notifOptions = {
+      //           type: "basic",
+      //           iconUrl: "icon48.png",
+      //           title: "Limit reached!",
+      //           message: "Uh oh, look's like you've reached your alloted limit."
+      //         };
+      //         chrome.notifications.create('limitNotif', notifOptions);
+
+      //       }
+      //     });
+      //   });
+      // }
+  //   }
+  // });
+
   // Export to window
   window.app = window.app || {};
   window.app.Controller = Controller;
